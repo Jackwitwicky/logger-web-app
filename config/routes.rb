4c973c00758ce8d874ce625086d0db0e1e3631ac
Jack Kiarie <jacknkiarie@gmail.com>
2020-02-11 15:31:02 +0300

    added dashboard gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :logs
+
+      root to: "users#index"
+    end
   resources :logs
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
